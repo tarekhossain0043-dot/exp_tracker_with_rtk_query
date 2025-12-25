8911c71330a0300d78b217b4483f9ef3d5a9ba4a
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-25 15:40:40 +0600

    feature/graph is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Graph from "./components/Graph";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-6xl px-2 m-5">
-      <h1 class="text-3xl font-semibold font-primary capitalize rounded-lg py-3 text-center bg-slate-800 text-white/80 mb-7">
+      <h1 className="text-3xl font-semibold font-primary capitalize rounded-lg py-3 text-center bg-slate-800 text-white/80 mb-7">
         Expense tracker
       </h1>
       {/* grid columns */}
       <div className="grid md:grid-cols-2">
         {/* chart */}
+        <div>
+          <Graph />
+        </div>
         {/* form */}
       </div>
     </div>
---
+++ client/src/components/Graph.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJs, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJs.register(ArcElement, Tooltip, Legend);
+
+const config = {
+  data: {
+    datasets: [
+      {
+        // label: "Budget Allocation",
+        data: [330, 240, 100],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverOffset: 10, // হোভার করলে স্লাইসটা একটু বড় হবে
+        borderRadius: 15,
+        spacing: 10,
+        borderWidth: 2,
+
+        cutout: "60%", // ডোনাটের মাঝখানের ফাঁকা অংশ বড় বা ছোট করার জন্য
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom", // লেজেন্ড নিচে দেখাবে
+        labels: {
+          color: "#333",
+          font: { size: 14 },
+        },
+      },
+    },
+  },
+};
+export default function Graph() {
+  return (
+    <div className="flex justify-center items-center max-w-sm mx-auto">
+      <div className="item relative">
+        <Doughnut {...config} />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col gap-2 items-center justify-center">
+          <span className="text-xl font-bold mb-0">Total</span>
+          <span className="text-emerald-500 text-2xl">${0}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
