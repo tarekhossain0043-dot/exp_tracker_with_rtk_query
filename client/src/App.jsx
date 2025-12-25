17384f5403936547bd16fc8e8902d14e7d4831ae
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-25 11:29:53 +0600

    feature/expence tracker head done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="container mx-auto max-w-6xl px-2 m-5">
+      <h1 class="text-3xl font-semibold font-primary capitalize rounded-lg py-3 text-center bg-slate-800 text-white/80 mb-7">
+        Expense tracker
+      </h1>
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2">
+        {/* chart */}
+        {/* form */}
+      </div>
     </div>
   );
 };
