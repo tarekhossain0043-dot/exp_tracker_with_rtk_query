f91f5c1978a6dfc86e201caf00aa1bf550898df1
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-26 08:19:23 +0600

    server created successfully

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 56798;
+const port = 5600;
 
-// middle ware
+// middleware
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
-app.listen(port, () => console.log(`server started with port : ${port}`));
+// run server
+
+app.listen(port, () => console.log(`server run on the port : ${port}`));
 
 // const express = require("express");
 // const app = express();
