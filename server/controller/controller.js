1eed668a2dcfa462d7d97996c516f3eb6cd97460
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-12-27 10:24:12 +0600

    feature/post transition data to mongodb database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,6 @@
 const model = require("../models/model");
 
-// post request to http server
+//--post-- http://localhost:8080/api/categories
 async function create_categories(req, res) {
   const create = new model.Category({
     type: "Hello",
@@ -16,6 +16,30 @@ async function create_categories(req, res) {
   }
 }
 
+//--get--http://localhost:8080/api/categories
+async function get_category(req, res) {
+  let data = await model.Category.find({});
+
+  // get data only of the type and color
+  let dynamicData = await data.map((v) =>
+    Object.assign({}, { type: v.type, color: v.color })
+  );
+  return res.json(dynamicData);
+}
+
+//--post--http://localhost:8080/api/transtions
+async function create_transitoin(req, res) {
+  if (!req.body) return res.status(400).json(`Post HTTP data not Provided`);
+  let { type, name, amout } = req.body;
+  const create = await new model.Transition({
+    type,
+    name,
+    amout,
+    date: new Date(),
+  });
+}
+
 module.exports = {
   create_categories,
+  get_category,
 };
--- server/routes/route.js
+++ server/routes/route.js
@@ -3,6 +3,9 @@ const routes = require("express").Router();
 // controller
 const controller = require("../controller/controller");
 
-routes.route("/api/categories").post(controller.create_categories);
+routes
+  .route("/api/categories")
+  .post(controller.create_categories)
+  .get(controller.get_category);
 
 module.exports = routes;
